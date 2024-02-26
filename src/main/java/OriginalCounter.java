f6bede97ef4188394fc75caf58fb9df907236dca
Alex <sahok200888@gmail.com>
2024-02-27 00:00:54 +0300

    First commit

--- src/main/java/OriginalCounter.java
+++ src/main/java/OriginalCounter.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 public class OriginalCounter {
     int countLetters(String str, char letter){
         int count = 0;
-        str = str.toLowerCase(Locale.ROOT);
+        str = str.toLowerCase();
         for(int i=0; i<str.length(); i++){
             if (str.charAt(i) == letter){
                 count+=1;
